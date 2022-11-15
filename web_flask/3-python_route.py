35e84c0a43754d67068538dc073416f3dcaf8af1
JumaOchi <ochiengjuma95@gmail.com>
2022-11-16 02:13:16 +0300

    update solution

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,34 +1,49 @@
 #!/usr/bin/python3
-"""
-Script that starts a Flask web application
-"""
+''' A simple flask app'''
 from flask import Flask
+from markupsafe import escape
 
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def hello_hbnb():
-    """view function"""
+def index():
+    '''Prints hello hbnb'''
     return 'Hello HBNB!'
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """view function"""
+    '''Prints hbnb'''
     return 'HBNB'
 
 
 @app.route('/c/<text>', strict_slashes=False)
-def c_text(text):
-    """view function"""
-    return 'C %s' % text.replace('_', ' ')
+def c_is_fun(text):
+    '''Prints c <text>'''
+    text = escape(text)
+    text_li = []
+    for i in text:
+        text_li.append(i)
+    for i in range(len(text_li)):
+        if text_li[i] == '_':
+            text_li[i] = ' '
+    text = ''.join(text_li)
+    return 'C ' + text
+
+
+@app.route('/python/', strict_slashes=False)
+def python_is_default():
+    '''Prints "Python is cool"'''
+    return 'Python is cool'
 
 
 @app.route('/python/<text>', strict_slashes=False)
-def python_text(text="is cool"):
-    """view function"""
-    return 'Python %s' % text.replace('_', ' ')
+def python_is_cool(text):
+    '''Prints c <text>'''
+    text = escape(text)
+    text = text.replace('_', ' ')
+    return 'Python ' + text
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
